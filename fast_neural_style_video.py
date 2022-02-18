cd68f447bf3eacf91d5f3edcf66c99f19a0ffae7
Aiden <18722067@lucbjtu.ac.uk>
2022-02-18 10:07:30 +0800

    修复视频功能的小问题

--- fast_neural_style_video.py
+++ fast_neural_style_video.py
@@ -1,14 +1,12 @@
 import time
-
 import cv2
 from utils.fast_neural_style_implementor import StyleImplementor
 
 # 读取视频
 cap = cv2.VideoCapture(0)
-
+# 载入模型
 implementor = StyleImplementor("models/instance_norm/udnie.t7", 400, 300)
-
-
+#视频流变换
 while True:
     # 接受键盘输入
     k = cv2.waitKey(1)
@@ -22,8 +20,8 @@ while True:
         break
     # implementor.show_video()
     # 灰度图像展示
-    out = implementor.show_gray_video(frame)
-    # 检测s键保存图片，图像路径为当前时间
+    out = implementor.show_video(frame)
+    # 检测s键保存图片，图像路径为当前时间，保存在capture文件夹下
     if k == ord('s'):
         filename = time.strftime("%Y%m%d%H%M%S", time.localtime()) + ".jpg"
         print(filename + " 已保存")
--- utils/fast_neural_style_implementor.py
+++ utils/fast_neural_style_implementor.py
@@ -5,7 +5,6 @@ class StyleImplementor:
 
     def __init__(self, model, width, height):
         """
-        :param source: 需要处理的图像/视频流
         :param width: 输出图像宽
         :param height: 输出图像高
         :param model: 模型地址
@@ -15,6 +14,7 @@ class StyleImplementor:
             self.net = cv2.dnn.readNetFromTorch(model)
             self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
         except Exception as e:
+            print(e)
             print("模型路径不正确，请检查传入的model参数！已采用默认模型")
             self.net = cv2.dnn.readNetFromTorch("models/instance_norm/udnie.t7")
             self.net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
@@ -22,19 +22,22 @@ class StyleImplementor:
         self.height = height
 
     def implementor(self, source):
+        # 去均值处理
         blob = cv2.dnn.blobFromImage(source, 1.0, (self.width, self.height), (103.939, 116.779, 123.680),
                                      swapRB=False, crop=False)
         # 计算/处理
         self.net.setInput(blob)
         out = self.net.forward()
         out = out.reshape(3, out.shape[2], out.shape[3])
+        # 还原
         out[0] += 103.939
         out[1] += 116.779
-        out[2] += 123.68
+        out[2] += 123.680
         out /= 255
         out = out.transpose(1, 2, 0)
         return out
 
+    # :param source: 需要处理的图像/视频流
     def show_img(self, source):
         cv2.imshow('Style Image', self.implementor(source))
         cv2.waitKey(0)
